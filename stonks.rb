c05413105c31956290afb56271c271a8125fde97
Eric Schwartz <ericschwartz7@gmail.com>
2021-01-30 11:46:55 -0500

    Show dates instead of "days ago". Some refactoring.

--- stonks.rb
+++ stonks.rb
@@ -1,10 +1,11 @@
 require "pry"
+require "date"
 require "httparty"
 require "csv"
 
 # puts "STONKS!"
 
-BASE_URL = "https://api.pushshift.io/reddit/search/submission/?q="
+BASE_URL = "https://api.pushshift.io/reddit/search/"
 
 ticker_table = CSV.parse(File.read("nyse.csv"), headers: true)
 ALL_TICKERS = ticker_table.map{|ticker_row| ticker_row["Symbol"]}
@@ -12,8 +13,10 @@ ALL_TICKERS = ticker_table.map{|ticker_row| ticker_row["Symbol"]}
 
 time_query = "&after=24h"
 
-def fetch(ticker, time_query)
-    url = "#{BASE_URL}#{ticker}#{time_query}&metadata=true&size=1&sort_by=created_utc&sort=desc"
+def fetch(ticker, time_query, comments = false)
+    type = comments ? "comment/?q=" : "submission/?q="
+    url = "#{BASE_URL}#{type}#{ticker}#{time_query}&metadata=true&size=1&sort_by=created_utc&sort=desc"
+    # puts url
     HTTParty.get(url)
 end
 
@@ -28,12 +31,15 @@ def print_first_result(response)
     puts response["data"][0]["selftext"]
 end
 
-def get_daily_change(ticker)
-    (1..20).each do |i|
+def get_daily_change(ticker, comments = false)
+    (1..30).each do |i|
         time_query = "&after=#{i}w&before=#{i - 1}w"
-        response = fetch(ticker, time_query)
+        response = fetch(ticker, time_query, comments)
         results = response["metadata"]["total_results"] unless !response["metadata"]
-        puts "#{ticker} #{i} weeks ago: #{results}"
+
+        date = (Date.today - (i * 7))
+
+        puts "#{ticker} #{date}: #{results}"
         sleep 0.7
     end
 end
@@ -60,7 +66,7 @@ def fetch_all_posts(time_query)
         fetch_posts(ticker, time_query)
         # get_daily_change(ticker, time_query)
     end
-end 
+end
 
 def fetch_all_comments(time_query)
     ALL_TICKERS.each do |ticker|
@@ -69,7 +75,7 @@ def fetch_all_comments(time_query)
 end
 
 # fetch_all_posts(time_query)
-get_daily_change("AMC")
+get_daily_change("GME", true)
 # fetch_all_comments("&after=12h")
 
 # If we want to compare day to day, we'll need to store results in a database.
